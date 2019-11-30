71fdbdd5bb5b882332643516211a5db40a3b88c7
Haider Ali <alihaider907@gmail.com>
2019-11-30 17:45:12 +0400

    enviornment variables for db configurations

--- api/server/models/index.js
+++ api/server/models/index.js
@@ -1,19 +1,12 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+import fs from 'fs'
+import path from 'path'
+import Sequelize from 'sequelize'
+const basename = path.basename(__filename)
+const env = 'development'
+import dbConfig from '../config/config'
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(dbConfig[env].database, dbConfig[env].username, dbConfig[env].password, dbConfig[env]);
 
 fs
   .readdirSync(__dirname)
